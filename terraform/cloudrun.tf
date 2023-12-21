bdfc489a023bb9c46a9eeadee7ee69df1c21e874
HaradaKumiko <fariv.fariv12@gmail.com>
2023-12-21 11:49:30 +0800

    change terraform

--- terraform/cloudrun.tf
+++ terraform/cloudrun.tf
@@ -3,31 +3,36 @@ resource "google_cloud_run_v2_service" "backend-api" {
   location = var.region
 
   template {
-    containers {
-      image = "asia.gcr.io/hexavator/friendease-backend@sha256:2379c7272c8061ee9f8c0aab7f5dcb646371019a6fbcd4ff8a9dcfbad7cc4451"
-
-      # Expose port 3000
-      ports {
-        container_port = 3000
+      containers {
+        image = "asia.gcr.io/hexavator/friendease-backend@sha256:2379c7272c8061ee9f8c0aab7f5dcb646371019a6fbcd4ff8a9dcfbad7cc4451"
       }
+    ports {
+          container_port = 9000
     }
+    scaling  {
+            # Min instances
+            min_instance_count  = 1
+            max_instance_count  = 3
+        }
 
-    scaling {
-      min_instances = 1
-      max_instances = 3
-    }
+    #   container_concurrency = 10
+    #   timeout_seconds       = 300
 
-    # Other configurations if needed
   }
+
+#   traffic {
+#     percent         = 100
+#     latest_revision = true
+#   }
+
 }
 
 resource "google_cloud_run_domain_mapping" "api_friendease_id" {
   name     = "api.friendease.id"
   location = var.region
-  metadata {
+    metadata {
     namespace = "${var.data-project}-api.friendease.id"
   }
-
   spec {
     route_name = google_cloud_run_v2_service.backend-api.name
   }
