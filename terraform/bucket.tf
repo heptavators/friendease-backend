716e99c8910d7dd37beb45ab686f24e1d386e360
HaradaKumiko <fariv.fariv12@gmail.com>
2023-12-21 14:22:27 +0800

    cloudbuild

--- terraform/bucket.tf
+++
@@ -1,8 +0,0 @@
-resource "google_storage_bucket" "testing-bucket" {
-  project = var.project
-  name = "${var.data-project}-testing-bucket"
-  force_destroy = false
-  uniform_bucket_level_access = true
-  location = var.region
-  labels = local.labels
-}
