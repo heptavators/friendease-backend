2a42eb386556f81fa3640315778c0d1fb946aa82
HaradaKumiko <fariv.fariv12@gmail.com>
2023-12-21 14:03:56 +0800

    cloudbuild

--- src/configs/Database/index.ts
+++ src/configs/Database/index.ts
@@ -3,14 +3,14 @@ import { Sequelize } from 'sequelize';
 
  const Database = new Sequelize({
   dialect:  'postgres',
-  database: 'friendease',
-  username: 'postgres',
-  password: 'postgres',
+  database: process.env.DATABASE_NAME || 'friendease',
+  username: process.env.DATABASE_USERNAME || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'postgres',
   host:    process.env.DATABASE_CONNECTION || '127.0.0.1',
   port: 5432,
   pool: {
-    max: 5,
-    min: 0,
+    max: 100,
+    min: 10,
     acquire: 30000,
     idle: 10000
   },
