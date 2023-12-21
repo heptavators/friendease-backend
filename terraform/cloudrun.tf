aba59d78a7b3eab870c735aeb7d972c56657b3c8
HaradaKumiko <fariv.fariv12@gmail.com>
2023-12-21 11:40:18 +0800

    change terraform

--- terraform/cloudrun.tf
+++ terraform/cloudrun.tf
@@ -4,7 +4,7 @@ resource "google_cloud_run_v2_service" "backend-api" {
 
   template {
       containers {
-        image = "asia.gcr.io/hexavator/friendease-backend"
+        image = "asia.gcr.io/hexavator/friendease-backend@sha256:2379c7272c8061ee9f8c0aab7f5dcb646371019a6fbcd4ff8a9dcfbad7cc4451"
       }
       scaling  {
             # Min instances
